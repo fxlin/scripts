639f7f6ec22f29a33ed39db48569efafdcd12e84
Felix Lin <xzl@rice.edu>
2015-03-16 22:14:50 -0400

    add android top scripts

---
+++ android/android.perf.sh
@@ -0,0 +1,42 @@
+#
+# run perf on android and associate the data with host symbols
+# in aosp.
+# ref: https://code.google.com/p/chromium/wiki/ProfilingContentShellOnAndroid
+
+###################################
+#	prepare kernel symbols
+###################################
+# Turn on ksyms
+# trick -- run echo as root
+adb shell echo 'echo 0 > /proc/sys/kernel/kptr_restrict' \| su
+# should be 0
+adb shell cat /proc/sys/kernel/kptr_restrict
+adb pull /proc/kallsyms /tmp/kallsyms
+# addresses should not be 00000
+head /tmp/kallsyms
+
+# push kersym to aosp tree
+scp /tmp/kallsyms office:/home/xzl/aosp/out/target/product/hammerhead/symbols/
+
+###################################
+#	record perf data: run on dev host
+###################################
+# Turn on ksyms
+# trick -- run echo as root
+adb shell echo 'echo 0 > /proc/sys/kernel/kptr_restrict' \| su
+# should be 0
+adb shell cat /proc/sys/kernel/kptr_restrict
+
+# -a: all cpus
+# -g: capture call graphs
+adb shell perf record -o /sdcard/perf.data -F 99 -ag -- sleep 10
+adb pull /sdcard/perf.data
+scp perf.data office:/tmp/
+
+###################################
+#	report perf data: run on office
+###################################
+cd /home/xzl/aosp/out/target/product/hammerhead
+../../../host/linux-x86/bin/perfhost report -g -i /tmp/perf.data --symfs symbols/ --kallsyms=symbols/kallsyms
+
+
